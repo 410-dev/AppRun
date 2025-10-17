62ccc1d1521195b368b555fce311f228199709d8
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-10-17 16:30:05 +0900

    Will send notification to GUI environment when installing/updating dependencies

--- src/usr/local/sbin/apprun-prepare.sh
+++ src/usr/local/sbin/apprun-prepare.sh
@@ -8,6 +8,7 @@ if [[ ! -d "$appBoxRoot" ]]; then
 fi
 
 if [[ ! -f "$1/main.py" ]] && [[ ! -f "$1/main.sh" ]] && [[ ! -f "$1/main.jar" ]] && [[ ! -x "$1/main" ]]; then
+    notify-send "[AppRun] App Preparation Failed" "No valid entry file found in $1"
     echo "No main.py, main.sh, main.jar, or executable main file found in $1. Skipping preparation."
     exit 9
 fi
@@ -35,20 +36,21 @@ if [ -f "$1/main.py" ]; then
         new_checksum=$(md5sum "$1/requirements.txt" | awk '{ print $1 }')
 
         if [[ "$old_checksum" == "" ]]; then
-            echo "$new_checksum" > "$appBoxRoot/$appid/requirements.txt.checksum"
             echo "First time setup, installing dependencies..."
             echo "Running preinstallation..."
+            notify-send "[AppRun] Installing Dependencies" "Installing dependencies for $appid. This may take a while."
             "$appBoxRoot/$appid/pyvenv/bin/python3" -m pip install --upgrade pip setuptools wheel
             "$appBoxRoot/$appid/pyvenv/bin/python3" -m pip install -r "$1/requirements.txt"
-
-        elif [[ "$old_checksum" != "$new_checksum" ]]; then
             echo "$new_checksum" > "$appBoxRoot/$appid/requirements.txt.checksum"
+        elif [[ "$old_checksum" != "$new_checksum" ]]; then
             echo "Requirements file changed, reinstalling dependencies..."
+            notify-send "[AppRun] Updating Dependencies" "Dependencies for $appid have changed. Reinstalling. This may take a while."
             rm -rf "$appBoxRoot/$appid/pyvenv"
             python3 -m venv "$appBoxRoot/$appid/pyvenv"
             echo "Running preinstallation..."
             "$appBoxRoot/$appid/pyvenv/bin/python3" -m pip install --upgrade pip setuptools wheel
             "$appBoxRoot/$appid/pyvenv/bin/python3" -m pip install -r "$1/requirements.txt"
+            echo "$new_checksum" > "$appBoxRoot/$appid/requirements.txt.checksum"
         fi
     fi
 fi
