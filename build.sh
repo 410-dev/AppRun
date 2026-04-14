a2c0ff07848cd0b9580290ad125c4bbfaf8923d8
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2026-04-14 16:03:21 +0900

    Renamed legacies

--- build.sh
+++ build.sh
@@ -2,6 +2,17 @@
 
 # If no argument, make deb file from src
 if [ -z "$1" ]; then
+
+    # Package src/usr/lib/AppRun/apprundropinf3.apprunxproj
+    if [[ ! -z "$(which apprun3-package)" ]]; then
+        apprun3-package src/usr/lib/AppRun/AppRunDropInService.apprunxproj --output src/usr/lib/AppRun/AppRunDropInService.apprunx
+    elif [[ ! -z "$(which apprun-package)" ]]; then
+        apprun-package src/usr/lib/AppRun/AppRunDropInService.apprunxproj --output src/usr/lib/AppRun/AppRunDropInService.apprunx
+    else
+        echo "Error: No apprun packaging tool found (apprun3-package or apprun-package)"
+        exit 1
+    fi
+
     sudo chown -R root:root src
     sudo chmod -R 755 src
     sudo dpkg-deb --build src
